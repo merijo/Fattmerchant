481834863c195d242ab130bd40f0b999040be880
Sandeep B Kadam <sandeep.bkadam9@gmail.com>
2020-08-06 21:17:45 +0530

    Add files via upload

---
+++ firstFile.js
@@ -0,0 +1,14 @@
+console.log("Hello World");
+var Name;
+function randuPrint(eee, aaa) {
+    console.log(eee, aaa);
+}
+var firstName = "Merin";
+var lastName = "Jose";
+randuPrint(lastName, firstName);
+console.log();
+function printLabel(labeledObj) {
+    console.log(labeledObj.label);
+}
+var myObj = { size: 10, label: firstName };
+printLabel(myObj);
